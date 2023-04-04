878e15d16cca7b73f750daaf11fb2d797cc86fb5
evad-e <cd.empinado@gmail.com>
2023-04-04 22:32:10 +0800

    Added search algorithm

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,20 @@ def search_users(args):
     """
 
     # Implement search here!
+    ret = []
+ 
+    if args.get('id') != None:
+       ret.extend(filter(lambda USERS: USERS['id'] == args.get('id'), USERS))
+    if args.get('name') != None:
+       ret.extend(filter(lambda USERS: args.get('name').lower() in USERS['name'].lower(), USERS))
+    if args.get('age') != None:
+       age = int(args.get('age'))
+       ret.extend(filter(lambda USERS: int(USERS['age']) <= age + 1 and int(USERS['age']) >= age - 1, USERS))
+    if args.get('occupation') != None:
+         ret.extend(filter(lambda USERS: args.get('occupation').lower() in USERS['occupation'].lower(), USERS))
+    
+    for i in range(len(ret)-1, 0, -1):
+        if ret[i] in ret[:i]:
+            del ret[i]
 
-    return USERS
+    return ret if args else USERS
